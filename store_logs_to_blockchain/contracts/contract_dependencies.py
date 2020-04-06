f4b1fbaa10bf049194b1216bb7cdbbd62f3d711f
Glenn <glenn-edgar@onyxengr.com>
2020-04-06 15:54:43 -0700

    daily commit

--- store_logs_to_blockchain/contracts/contract_dependencies.py
+++
@@ -1,4 +0,0 @@
-# valid types are "address" "int" and "string"
-contract_construction_parameters = {}
-contract_construction_parameters["EventHandler"] = []
-contract_construction_parameters["HelloWorld"] = [{"type":"address", "value":"EventHandler"}]
